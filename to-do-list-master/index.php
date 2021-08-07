79dfc1251ee406afc7b12a8421d5ca42be249420
minnuvarughese <minnuv40@gmail.com>
2021-08-07 18:50:02 +0530

    First Commit

---
+++ to-do-list-master/index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>
+login system
+</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<div>
+<h1 id="h">TO-DO LIST</h1>
+<a href="signup.php">
+    <p><span class="bg"></span><span class="base"></span><span class="text">SIGN UP</span></p></a>
+<a class="transparent" href="login.php">
+    <p><span class="bg"></span><span class="base"></span><span class="text">SIGN IN</span></p></a>
+</div>
+</body>
+</html>
